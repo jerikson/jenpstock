d5f8903b8ec1f80427e6320e1632e8fc4ae6a255
Jimmy Erikson <jimmyerikson@gmail.com>
2016-11-15 10:26:11 +0100

    Formatting code, cleaning up.

--- jenpstock/Controllers/DefaultController.cs
+++ jenpstock/Controllers/DefaultController.cs
@@ -1,88 +1,44 @@
 ﻿using System;
 using System.IO;
 using System.Net;
-using System.Text;
 using System.Web.Mvc;
-using System.Web.Script.Serialization;
-using System.Diagnostics;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace jenpstock.Controllers
 {
     public class DefaultController : Controller
     {
-        internal string Url = "";
+        internal string Url = "http://one.dev.parttrap.com/catalog/getrelatedchildproducts/?stockCode=";
         internal string StockId = "GOOGLE&relationId=4";
-
 
         public ActionResult Index()
         {
             List<Model.Product> productList = GetProduct(Url, StockId);
-
-            ViewBag.Title = Url;
             ViewBag.Stockcode = StockId;
-            
             return View(productList);
         }
 
         public List<Model.Product> GetProduct(string url, string stockId)
         {
-            List<JToken> productList = new List<JToken>();
+            List<Model.Product> productList = new List<Model.Product>();
             WebRequest request = WebRequest.Create(url + stockId);
             Stream dataStream = request.GetResponse().GetResponseStream();
             StreamReader reader = new StreamReader(dataStream);
-
             string response = reader.ReadToEnd();
             JArray jsonObj = (JArray)JsonConvert.DeserializeObject(response);
 
-            List<Model.Product> listan = new List<Model.Product>();
-
-            int index = 0;
-            foreach (var item in jsonObj)
+            for (var i = 0; i < jsonObj.Count; i++)
             {
-                Model.Product a = (Model.Product)JsonConvert.DeserializeObject(jsonObj[index].ToString(), typeof(Model.Product));
-                listan.Add(a);
-                index++;
+                Model.Product a = (Model.Product)JsonConvert.DeserializeObject(jsonObj[i].ToString(), typeof(Model.Product));
+                productList.Add(a);
             }
-
-            return listan;
+            return productList;
         }
     }
-
-
 }
 
-
-
-
-
-
-
-
-
-
-
-//public JToken GetProduct(string url, string stockId)
-//{
-//    WebRequest request = WebRequest.Create(url + stockId);
-//    Stream dataStream = request.GetResponse().GetResponseStream();
-//    StreamReader reader = new StreamReader(dataStream);
-
-//    string response = reader.ReadToEnd();
-//    JArray jsonObj = (JArray)JsonConvert.DeserializeObject(response);
-
-//    Debug.WriteLine("Key" + "\t\t" + "Value");
-
-//    foreach (var item in jsonObj)
-//    {
-//        Debug.WriteLine(item["ProductID"].ToString());
-//    }
-
-//    return jsonObj[0];
-//}
 
 
 
