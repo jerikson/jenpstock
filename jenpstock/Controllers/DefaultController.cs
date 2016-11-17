2f5bb82b9aa45d85037d5bd368dfd4ac5dbb710b
Jimmy Erikson <jimmyerikson@gmail.com>
2016-11-17 16:12:24 +0100

    Using WebClient instead of WebRequest.

--- jenpstock/Controllers/DefaultController.cs
+++ jenpstock/Controllers/DefaultController.cs
@@ -1,41 +1,28 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Net;
 using System.Web.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
+using ParttrapDev.Models;
+using System.Linq;
 
 namespace jenpstock.Controllers
 {
     public class DefaultController : Controller
     {
-        internal string Url = "http://one.dev.parttrap.com/catalog/getrelatedchildproducts/?stockCode=";
-        internal string StockId = "GOOGLE&relationId=4";
-
+        GoogleApi google = new GoogleApi();
+        
+        internal string Url = "";
+        internal string StockId = "";
+       
         public ActionResult Index()
         {
-            List<Model.Product> productList = GetProduct(Url, StockId);
+            JArray productList = google.ProductGet(Url);
             ViewBag.Stockcode = StockId;
             return View(productList);
         }
 
-        public List<Model.Product> GetProduct(string url, string stockId)
-        {
-            List<Model.Product> productList = new List<Model.Product>();
-            WebRequest request = WebRequest.Create(url + stockId);
-            Stream dataStream = request.GetResponse().GetResponseStream();
-            StreamReader reader = new StreamReader(dataStream);
-            string response = reader.ReadToEnd();
-            JArray jsonObj = (JArray)JsonConvert.DeserializeObject(response);
-
-            for (var i = 0; i < jsonObj.Count; i++)
-            {
-                Model.Product a = (Model.Product)JsonConvert.DeserializeObject(jsonObj[i].ToString(), typeof(Model.Product));
-                productList.Add(a);
-            }
-            return productList;
-        }
     }
 }
 
---
+++ jenpstock/Models/GoogleApi.cs
@@ -0,0 +1,76 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.ShoppingContent.v2;
+using Google.Apis.ShoppingContent.v2.Data;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System;
+using System.Linq;
+using jenpstock;
+
+namespace ParttrapDev.Models
+{
+    public class GoogleApi
+    {
+        private UserCredential _credential;
+        private ShoppingContentService _service;
+        private string _clientId;
+        private string _clientSecret;
+
+
+        public ShoppingContentService CreateService(UserCredential credential)
+        {
+            var service = new ShoppingContentService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "jenpbiz",
+            });
+            return service;
+        }
+
+        public JArray ProductGet(string url)
+        {
+            WebClient client = new WebClient();
+            JArray jsonObj = new JArray();
+            try
+            {
+                string json = client.DownloadString(url);
+                jsonObj = (JArray)JsonConvert.DeserializeObject(json);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Message: " + e.Message + "\nStacktrace" + e.StackTrace + "\nTarget: " + e.TargetSite);
+            }
+            finally
+            {
+                //client.Dispose();
+            }
+            return jsonObj;
+        }
+
+
+
+
+        public void ProductInsert()
+        {
+
+        }
+
+
+        public void ProductUpdate()
+        {
+
+        }
+
+        public void ProductDelete()
+        {
+
+        }
+
+    }
+}
+
+
